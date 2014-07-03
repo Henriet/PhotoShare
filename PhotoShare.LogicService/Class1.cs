45301a27b031b8636e8256a918ea8653df551597
Henriet <kuhar.elen@gmail.com>
2014-07-03 23:01:13 +0300

    Initial commit

---
+++ PhotoShare.LogicService/Class1.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PhotoShare.LogicService
+{
+    /// <summary>
+    /// 
+    /// папка для классов, реализующие репозитории. референс на нижний (3)
+    /// </summary>
+    public class Class1
+    {
+    }
+}
---
+++ PhotoShare.Service/Class1.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PhotoShare.Service
+{
+    /// <summary>
+    /// папка для ef. папка для доменной модели (сущностей). папка репозиториев.
+    /// </summary>
+    public class Class1
+    {
+    }
+}
